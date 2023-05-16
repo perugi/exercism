c4ccba25a02be6396dde0f5130f032d14935f73b
perugi <dominik.perusko@gmail.com>
2023-05-16 22:42:56 +0200

    Add solution to the Gigasecond exercise

---
+++ javascript/gigasecond/gigasecond.js
@@ -0,0 +1,8 @@
+//
+// This is only a SKELETON file for the 'Gigasecond' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const gigasecond = (startDate) => {
+  return new Date(startDate.getTime() + 1000000000 * 1000);
+};
