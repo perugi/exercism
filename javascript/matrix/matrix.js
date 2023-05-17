97c9881a65b53f8dabab371e14eaffe17018a20e
perugi <dominik.perusko@gmail.com>
2023-05-17 23:03:25 +0200

    Add solution to matrix exercise

---
+++ javascript/matrix/matrix.js
@@ -0,0 +1,26 @@
+//
+// This is only a SKELETON file for the 'Matrix' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export class Matrix {
+  constructor(inputMatrix) {
+    this.inputMatrix = inputMatrix;
+    this._rows = this.inputMatrix
+      .split("\n")
+      .map((row) => row.split(" ").map((character) => Number(character)));
+    this._columns = this._transposeArray(this._rows);
+  }
+
+  get rows() {
+    return this._rows;
+  }
+
+  get columns() {
+    return this._columns;
+  }
+
+  _transposeArray(array) {
+    return array[0].map((_, colIndex) => array.map((row) => row[colIndex]));
+  }
+}
