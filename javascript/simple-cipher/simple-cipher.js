2d52c72131c64f8c88c161d681cf29a447df1292
perugi <dominik.perusko@gmail.com>
2023-05-27 23:06:38 +0200

    Add solution to the cipher exercise

--- javascript/simple-cipher/simple-cipher.js
+++ javascript/simple-cipher/simple-cipher.js
@@ -4,22 +4,42 @@
 //
 
 export class Cipher {
-  constructor() {
-    self._key = key ?? "xxxxxxxxxx";
+  constructor(key) {
+    this._key = key ?? this._generateRandomKey();
   }
 
-  encode(plaintext) {
-    let ciphertext = "";
-    for (let i = 0; i < plaintext.length; i++) {
-      ciphertext += plaintext[i];
+  encode(plainText) {
+    return plainText.split("").reduce((cipherText, plainChar, i) => {
+      let cipherShift =
+        this._key.charCodeAt(i % this._key.length) - "a".charCodeAt(0);
+      let cipherCharCode = plainChar.charCodeAt(0) + cipherShift;
+      if (cipherCharCode > "z".charCodeAt(0)) cipherCharCode -= 26;
+
+      return cipherText + String.fromCharCode(cipherCharCode);
+    }, "");
+  }
+
+  decode(cipherText) {
+    return cipherText.split("").reduce((plainText, cipherChar, i) => {
+      let cipherShift =
+        this._key.charCodeAt(i % this._key.length) - "a".charCodeAt(0);
+      let plainCharCode = cipherChar.charCodeAt(0) - cipherShift;
+      if (plainCharCode < "a".charCodeAt(0)) plainCharCode += 26;
+
+      return plainText + String.fromCharCode(plainCharCode);
+    }, "");
+  }
+
+  _generateRandomKey(length = 10) {
+    let key = "";
+    for (let i = 0; i < length; i++) {
+      key += String.fromCharCode(Math.floor(Math.random() * 26) + 97);
     }
-  }
 
-  decode(ciphertext) {
-    throw new Error("Remove this statement and implement this function");
+    return key;
   }
 
   get key() {
-    return self._key;
+    return this._key;
   }
 }
