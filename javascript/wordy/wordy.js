277087d4a36a7ab1d9b8a4e525b947618dcef9e3
perugi <dominik.perusko@gmail.com>
2023-05-30 00:38:12 +0200

    Implemented everything except syntax errors

--- javascript/wordy/wordy.js
+++ javascript/wordy/wordy.js
@@ -1,13 +1,20 @@
-//
 // This is only a SKELETON file for the 'Wordy' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
 
 export const answer = (input) => {
-  let questionRegex =
-    /^What is (d+)(( plus | minus | multiplied by | divided by )(d+))*?$/;
+  let baseRegex = /^What is (-?\d+).*?/;
+  let operationsRegex = /(?:(plus|minus|multiplied by|divided by) (-?\d+))+/g;
 
-  const matches = input.match(questionRegex);
+  let calculation = Number(baseRegex.exec(input)[1]);
+  const operations = input.matchAll(operationsRegex);
 
-  console.log(matches);
+  for (let operation of operations) {
+    if (operation[1] === "plus") calculation += Number(operation[2]);
+    if (operation[1] === "minus") calculation -= Number(operation[2]);
+    if (operation[1] === "multiplied by") calculation *= Number(operation[2]);
+    if (operation[1] === "divided by") calculation /= Number(operation[2]);
+  }
+
+  return calculation;
 };
