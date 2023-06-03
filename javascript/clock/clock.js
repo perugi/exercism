c15bbb1eb1b3a6c1e45b15a6ad3981bb91183136
perugi <dominik.perusko@gmail.com>
2023-06-03 23:23:42 +0200

    Intermediate solution to exercise

--- javascript/clock/clock.js
+++ javascript/clock/clock.js
@@ -4,23 +4,50 @@
 //
 
 export class Clock {
-  constructor() {
-    throw new Error('Remove this statement and implement this function');
+  constructor(hours = 0, minutes = 0) {
+    console.log(`Input: ${hours}, ${minutes}`);
+    this.hours = (hours + Math.floor(minutes / 60)) % 24;
+    this.minutes = minutes % 60;
+    console.log(`Intermediate: ${this.hours}, ${this.minutes}`);
+
+    this._rolloverClock();
+    console.log(`Output: ${this.hours}, ${this.minutes}`);
   }
 
   toString() {
-    throw new Error('Remove this statement and implement this function');
+    return `${this.hours.toString().padStart(2, "0")}:${this.minutes
+      .toString()
+      .padStart(2, "0")}`;
   }
 
-  plus() {
-    throw new Error('Remove this statement and implement this function');
+  plus(minutes) {
+    this.hours = (this.hours + Math.floor((this.minutes + minutes) / 60)) % 24;
+    this.minutes = (this.minutes + minutes) % 60;
+
+    return new Clock(this.hours, this.minutes);
   }
 
-  minus() {
-    throw new Error('Remove this statement and implement this function');
+  minus(minutes) {
+    this.hours = (this.hours - Math.floor(this.minutes - minutes / 60)) % 24;
+    this.minutes = (this.minutes - minutes) % 60;
+
+    this._rolloverClock();
+
+    return new Clock(this.hours, this.minutes);
   }
 
   equals() {
-    throw new Error('Remove this statement and implement this function');
+    throw new Error("Remove this statement and implement this function");
+  }
+
+  _rolloverClock() {
+    if (this.hours < 0) {
+      this.hours += 24;
+    }
+
+    if (this.minutes < 0) {
+      this.minutes += 60;
+      this.hours -= 1;
+    }
   }
 }
