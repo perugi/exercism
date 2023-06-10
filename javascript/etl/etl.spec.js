507b8cc6b2f15846b65a7b0d8a9ac92c39183ba6
perugi <dominik.perusko@gmail.com>
2023-06-10 14:11:17 +0200

    Add changes to test spec

--- javascript/etl/etl.spec.js
+++ javascript/etl/etl.spec.js
@@ -75,3 +75,4 @@ describe("Transform legacy to new", () => {
     expect(transform(old)).toEqual(expected);
   });
 });
+Write a function that loops through the array and displays each book on the page. You can display them in some sort of table, or each on their own “card”. It might help for now to manually add a few books to your array so you can see the display.
